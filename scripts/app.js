1b6a007b43e29123eef225bd5da008fc70ae3f01
Carl Furrow <carl.furrow@gmail.com>
2014-12-18 16:44:39 -0800

    simplify

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,9 @@
 var googleImageSearch = new google.search.ImageSearch();
-googleImageSearch.setResultSetSize(8); // 8 per page is max
-var searcher = new ImageSearcher();
 var done = false;
 
-var doGoogleImageSearch = function(query) {
-  function doneSearching() {
-    var page = Math.floor(googleImageSearch.cursor.pages.length * Math.random());
-    if(done === false) {
-      googleImageSearch.gotoPage(page);
-      done = true;
-    }
-    searcher.searchComplete(googleImageSearch.results);
-  }
-  googleImageSearch.setSearchCompleteCallback(this, doneSearching, null);
-  googleImageSearch.execute(query);
-}
+googleImageSearch.setResultSetSize(8); // 8 per page is max
 
-searcher.processResults = function(results) {
+var displayResults = function(results) {
   var resultArea = document.getElementById("result");
   var len = results.length;
   var i = 0;
@@ -33,4 +20,19 @@ searcher.processResults = function(results) {
   }
 };
 
-searcher.search("chicken noodle soup", doGoogleImageSearch)
+
+var performGoogleImageSearch = function(query) {
+  function doneSearching() {
+    var page = Math.floor(googleImageSearch.cursor.pages.length * Math.random());
+    if(done === false) {
+      googleImageSearch.gotoPage(page);
+      done = true;
+    }
+    displayResults(googleImageSearch.results);
+  }
+  googleImageSearch.setSearchCompleteCallback(this, doneSearching, null);
+  googleImageSearch.execute(query);
+
+}
+
+performGoogleImageSearch("chicken noodle soup")
